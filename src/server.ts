cba48b05560d9a19725f07995d7b1fee0b1e858c
Pietro <pietro.dsantos77@gmail.com>
2024-10-21 22:47:09 -0300

    Ajustando prisma generate em prod4

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,16 @@ import { mainRouter } from "./routers/main";
 import path from "path";  // Importar o path para gerar o caminho correto
 
 const server = express();
+
+// Configurar Helmet para segurança
 server.use(helmet());
+
+// Configuração do CORS para permitir todas as origens
 server.use(cors({
-    origin: '*',
-    credentials: true,
+    origin: '*',  // Permite todas as origens
 }));
+
+// Middleware para parsing de dados
 server.use(urlencoded({ extended: true }));
 server.use(express.json());
 
@@ -19,6 +24,11 @@ server.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 // Rotas
 server.use(mainRouter);
 
-server.listen(process.env.PORT || 3001, () => {
-    console.log(`Server is running ${process.env.BASE_URL}`);
+// Inicialização do servidor
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    if (process.env.BASE_URL) {
+        console.log(`Base URL: ${process.env.BASE_URL}`);
+    }
 });
